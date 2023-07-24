41a914699d50061ec355d62c9f74fa2fd2067fdc
cclarke74 <clairc2@gmail.com>
2023-07-24 09:13:33 -0700

    first commit

---
+++ app.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title: string = "Data Binding Examples";
+  firstName: string = "Jack";
+	lastName: string = "Martin";
+
+    isDisabled: boolean = true;
+
+    constructor(){
+      setTimeout(()=>{
+        this.isDisabled = false;
+      },2000);
+    }
+
+    getName(): string{
+      return this.firstName + ' ' + this.lastName;
+    }
+
+    getIsDisabled(): boolean {
+      return this.isDisabled;
+    }
+    }
+  
